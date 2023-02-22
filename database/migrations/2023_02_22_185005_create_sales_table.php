04bf5c50fecae5d4c44d3a796587a27076dfea29
eardakbekov1 <eardakbekov@gmail.com>
2023-02-23 01:24:39 +0600

    migrations created

---
+++ database/migrations/2023_02_22_185005_create_sales_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->date('sale_date');
+            $table->integer('client_id');
+            $table->integer('flat_id');
+            $table->integer('house_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
---
+++ database/migrations/2023_02_22_185114_create_flats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFlatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('flats', function (Blueprint $table) {
+            $table->id();
+            $table->integer('floor');
+            $table->float('square');
+            $table->integer('number_of_rooms');
+            $table->integer('house_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('flats');
+    }
+}
